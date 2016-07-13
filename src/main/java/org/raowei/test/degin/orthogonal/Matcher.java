9f7b34f9b943980e776760e8e8a467e1e88d671c
terryrao <137469949@qq.com>
2016-07-14 00:40:38 +0800

    提取函数消除重复

--- src/main/java/org/raowei/test/degin/orthogonal/Matcher.java
+++ src/main/java/org/raowei/test/degin/orthogonal/Matcher.java
@@ -18,4 +18,29 @@ public interface Matcher<T> {
         return actual -> actual.equals(expected);
     }
 
+    /**
+     * 可以消除重复
+     * @param expected
+     * @param <T>
+     * @return
+     */
+    static <T extends Comparable<? super T>> Matcher<T> gt(T expected) {
+        return actual -> Ordering.<T>natural().compare(actual,expected) > 0;
+    }
+
+    static <T extends Comparable<? super T>> Matcher<T> lt(T expected) {
+        return actual -> Ordering.<T>natural().compare(actual,expected) < 0;
+    }
+
+    static <T extends Comparable<? super T>> Matcher<T> gt1(T expected) {
+        return  actual -> compare(actual,expected) > 0;
+    }
+
+    static <T extends Comparable<? super T>> Matcher<T> lt2(T expected)
+    {
+        return actual -> compare(actual,expected) < 0;
+    }    static <T extends Comparable<? super T >> int compare(T t1, T t2) {
+        return Ordering.<T>natural().compare(t1,t2);
+    }
+
 }
