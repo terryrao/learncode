bde79be989d383559f48130fdf321b2e592ca2b9
raowei <raowei@88bank.com>
2016-07-22 18:17:35 +0800

    junit  in action  jmocks

---
+++ src/main/java/org/raowei/test/pattern/interceptor/Interceptor.java
@@ -0,0 +1,14 @@
+package org.raowei.test.pattern.interceptor;
+
+/**
+ * ${DESCRIPTION}
+ * create: 2016-07-22 18:16
+ *
+ * @author admin
+ */
+public interface Interceptor {
+    void interceptBefore();
+
+    void interceptAfter();
+
+}
