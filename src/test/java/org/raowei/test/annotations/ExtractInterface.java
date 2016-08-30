b65b9f7eafc9b1c26735ede45ab63bcdbd9f3c48
raowei <raowei@88bank.com>
2016-08-30 17:06:46 +0800

    nio

--- src/test/java/org/raowei/test/annotations/ExtractInterface.java
+++ src/test/java/org/raowei/test/annotations/ExtractInterface.java
@@ -2,13 +2,10 @@ package org.raowei.test.annotations;
 
 import java.lang.annotation.*;
 
-/**
- * Created by terryrao on 6/7/2015.
- */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
 @Documented
 public @interface ExtractInterface {
-    public String value();
+    String value();
 
 }
